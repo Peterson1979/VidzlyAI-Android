6dc11b9165c48b04a6138be4cc4041aba321f3c6
Oberhauser Péter Zoltán <lifesoffers@gmail.com>
2025-06-19 08:20:17 +0200

    Trigger deployment after Vercel connection

--- api/gemini/summarize.js
+++ api/gemini/summarize.js
@@ -2,7 +2,7 @@
 export default function handler(request, response) {
   // Later this will call the YouTube API
   response.status(200).json({
-    message: "This endpoint will fetch popular YouTube videos.",
+    message: "This endpoint will fetch popular YouTube videos summarize.",
     path: "/api/youtube/popular"
   });
 }
