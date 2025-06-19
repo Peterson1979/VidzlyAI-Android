0322496826657b9158b05f3fc7000cc8ed5b5b9e
Oberhauser Péter Zoltán <lifesoffers@gmail.com>
2025-06-19 07:54:02 +0200

    Trigger deployment after Vercel connection

--- api/youtube/search.js
+++ api/youtube/search.js
@@ -3,7 +3,7 @@
 export default function handler(request, response) {
   // Later this will call the YouTube API
   response.status(200).json({
-    message: "This endpoint will fetch popular YouTube videos.",
+    message: "This endpoint will fetch popular YouTube videos search.",
     path: "/api/youtube/popular"
   });
 }
